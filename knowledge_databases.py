d3f4f3e5d2a0d6bda15198db56e3ca9e32a62da5
myname21-meet <myname21@meet.mit.edu>
2019-07-29 07:12:24 -0400

    finished th elabs!

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -1,4 +1,4 @@
-from knowledge_model import Base, Knowledge
+from knowledge_model import Base, Animal
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,17 +8,36 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name,life_span,has_wings,rating):
+	add_article1 = Animal(name=name,
+		life_span=life_span, 
+		has_wings=has_wings,
+		rating=rating)
+	session.add(add_article1)
+	session.commit()
+add_article("elephant",60,False,6)
+add_article("horse",100,False,9)
 
 def query_all_articles():
-	pass
+	animal_all=session.query(Animal).all()
+	return animal_all
 
-def query_article_by_topic():
-	pass
+print(query_all_articles())
 
-def delete_article_by_topic():
-	pass
+
+
+def query_article_by_topic(topic):
+	chosen_topic=session.query(Animal).filter_by(name=topic).all()
+	return chosen_topic
+
+print(query_article_by_topic("elephant"))
+
+
+def delete_article_by_topic(name):
+	session.query(Animal).filter_by(
+       name=name).delete()
+	session.commit()
+delete_article_by_topic("elephant")
 
 def delete_all_articles():
 	pass
--- knowledge_model.py
+++ knowledge_model.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Knowledge(Base):
+class Animal(Base):
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -13,5 +13,19 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	__tablename__='animals'
+	animals_type_number=Column(Integer, primary_key=True)
+	name = Column(String)
+	life_span=Column(Integer)
+	has_wings=Column(Boolean)
+	rating=Column(Integer)
 
-	pass
+	def __repr__(self):
+		return ("Animal Type: {}\n"
+				"Animal Life Span: {} \n"
+				"Has Wings?: {}\n"
+				"Rating: {}").format(
+                    self.name, self.life_span, self.has_wings, self.rating)
+
+
+	
